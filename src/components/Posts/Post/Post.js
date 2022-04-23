b6d1d97326a3844cc866191015733a3d87f1c0fc
AnadiSharma007 <anadis764@gmail.com>
2022-04-23 17:07:39 +0530

    added mobile view and constants

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -32,12 +32,12 @@ function Post({ post, setCurrentId }) {
       </div>
          <Typography className={classes.title} variant='h5' gutterBottom>{post.title}</Typography>
        <CardContent>
-         <Typography variant='body1' gutterBottom>{post.message}</Typography>
+         <Typography variant='body2' color='textSecondary' component="p">{post.message}</Typography>
        </CardContent>
        <CardActions className={classes.cardActions}>
          <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}>
            <ThumbUpAltIcon fontSize='small' />
-           Like
+           &nbsp; Like &nbsp;
            {post.likeCount}
          </Button>
          <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
