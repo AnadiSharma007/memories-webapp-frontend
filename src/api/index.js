73cd979f6f9de2c014d1d3fac576f97d068147db
AnadiSharma007 <anadis764@gmail.com>
2022-04-30 10:32:10 +0530

    made changes

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'http://localhost:5000' });
 
-
-
 export const fetchPosts = () => API.get('/posts'); 
 export const createPost = (newPost) => API.post('/posts', newPost);
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
