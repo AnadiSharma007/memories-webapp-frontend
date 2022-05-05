866ada18b3fbee1ebeae237cdcbb8cc6bc8449f1
AnadiSharma007 <anadis764@gmail.com>
2022-05-05 20:25:26 +0530

    updated like and delete functionality

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,7 +8,6 @@ import { createPost, updatePost } from "../../actions/posts"
 
 function Form({ currentId, setCurrentId }) {
     const [postData, setPostData] = useState({
-      creator: '',
       title: '',
       message: '',
       tags: '',
@@ -19,6 +18,7 @@ function Form({ currentId, setCurrentId }) {
 
     const classes = useStyles(); 
     const dispatch = useDispatch();
+    const user = JSON.parse(localStorage.getItem('profile'));
 
     useEffect(() => {
       if(post) setPostData(post);
@@ -28,10 +28,10 @@ function Form({ currentId, setCurrentId }) {
       e.preventDefault();
 
       if(currentId){
-        dispatch(updatePost(currentId, postData));
+        dispatch(updatePost(currentId, { ...postData,  name: user?.result?.name}));
       }
       else{
-        dispatch(createPost(postData));
+        dispatch(createPost({ ...postData, name: user?.result?.name}));
       }
 
       clear();
@@ -40,7 +40,7 @@ function Form({ currentId, setCurrentId }) {
 
     const clear = () => {
       setCurrentId(null);
-      setPostData({ creator: '',
+      setPostData({
       title: '',
       message: '',
       tags: '',
@@ -48,11 +48,22 @@ function Form({ currentId, setCurrentId }) {
       })
     }
 
+
+    if(!user?.result?.name){
+      return(
+        <Paper className={classes.paper}>
+          <Typography variant='h6' align='center'>
+            Please Sign in to create your own memories and like other's memories
+          </Typography>
+        </Paper>
+      )
+    }
+
+
   return (
     <Paper className={classes.paper}>
       <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
       <Typography variant="h6">{currentId ? 'Editing' : 'Creating'} a Memory</Typography>
-      <TextField name='creator' variant='outlined' label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({...postData, creator: e.target.value})}/>
       <TextField name='title' variant='outlined' label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({...postData, title: e.target.value})}/>
       <TextField name='message' variant='outlined' label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({...postData, message: e.target.value})}/>
       <TextField name='tags' variant='outlined' label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({...postData, tags: e.target.value.split(",")})}/>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,8 @@
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
 import { LOGOUT } from '../../constants/actionTypes'
 import React, { useState, useEffect } from 'react';
-import { Link, useHistory, useLocation } from 'react-router-dom';
+import decode from 'jwt-decode';
+import { Link, useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import useStyles from './styles';
 import memories from '../../images/memories.png'
@@ -11,7 +12,6 @@ const Navbar = () => {
     const classes = useStyles();
     const dispatch = useDispatch();
     const history = useHistory();
-    const location = useLocation();
 
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
 
@@ -26,11 +26,15 @@ const Navbar = () => {
     useEffect(() => {
       const token = user?.token;
 
-      //JWT...
+      if(token){
+        const decodedToken = decode(token);
+
+        if(decodedToken.exp * 1000 < new Date().getTime()) logout();
+      }
 
       setUser(JSON.parse(localStorage.getItem('profile')));
 
-    }, [location]);
+    }, [user.token, logout]);
 
   return (
     <AppBar className={classes.appBar} position='static' color='inherit'>
